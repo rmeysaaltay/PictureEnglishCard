1c421c968a3414576c5164e30e0e89c33d9c8279
rmeysaaltay <altayrumeysa9@gmail.com>
2024-09-09 12:14:16 +0300

    first commit

---
+++ app/src/main/java/com/example/pictureenglishcards/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.pictureenglishcards
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pictureenglishcards.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
